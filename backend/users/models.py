556aede7d2a5648b5b54b3d8ce3b6a68b174c80e
Subash Chandra Bose G <subashdev070@gmail.com>
2026-06-30 08:46:33 +0530

    Implement custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    ROLE_CHOICES = (
+        ("ADMIN", "Admin"),
+        ("CUSTOMER", "Customer"),
+    )
+
+    name = models.CharField(
+        max_length=150
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="CUSTOMER"
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
